ecc401b4d116f672a394ff6502c629db3fac270e
Peter <peter@hertigcarl.se>
2023-12-06 00:02:12 +0100

    lade till deployscript för s3

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+BUCKET_NAME=$1
+aws s3 sync --exclude '.git/*' --exclude deploy.sh . "s3://$BUCKET_NAME"
+aws s3api put-object-acl --bucket $BUCKET_NAME --key index.html --acl public-read
+aws s3api put-object-acl --bucket $BUCKET_NAME --key thumb.webp --acl public-read
+
+aws s3 website "s3://$BUCKET_NAME" --index-document index.html
+echo "http://$BUCKET_NAME.s3-website.eu-north-1.amazonaws.com/"
